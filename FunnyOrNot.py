87009ba59cc9d09aa283407e9b34a10cff30ab7c
AlanTheBlank <alanoreilly@outlook.ie>
2020-06-13 13:08:01 +0100

    Fixed FunnyOrNot.py

--- FunnyOrNot.py
+++ FunnyOrNot.py
@@ -1,11 +1,12 @@
 import os
+import sys
 
 funny = False
 # Holy sh*t are you actually gonna run this?
-choice = input("WARNING: This program will scan your files in the current working directory and delete anything not funny.  Type 'Yes' to continue")
-if('yes' in choice):
+choice = input("WARNING: This program will scan your files in the current working directory and delete anything not funny.  Type 'Yes' to continue\n")
+if('yes'.upper() in choice.upper()):
     for x in os.listdir("."):
-        if(not os.path.isdir(x)):
+        if(not os.path.isdir(x) and x != sys.argv[0]):
             if(not funny):
                 f = open(x)
                 print("bruh " + x + " isn't funny")
